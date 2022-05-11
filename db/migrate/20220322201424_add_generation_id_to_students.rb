e719ad86b62d9478b66e2552c17b18287f2913c5
monicablanc <mblanc72@hotmail.com>
2022-05-11 13:01:11 -0500

    Arreglar mi desmadre -parte 3

--- db/migrate/20220322201424_add_generation_id_to_students.rb
+++ db/migrate/20220322201424_add_generation_id_to_students.rb
@@ -1,9 +1,9 @@
 class AddGenerationIdToStudents < ActiveRecord::Migration[6.1]
   def change
     # add_reference :students, :generation, null: false, foreign_key: true
-    add_column :students, :ClaveCurp, :string, limit: 18
-    add_column :students, :num_control, :integer, null: false, unique: true
-    add_index :students, :num_control
+    # add_column :students, :ClaveCurp, :string, limit: 18
+    # add_column :students, :num_control, :integer, null: false, unique: true
+    # add_index :students, :num_control
 
   end
 end
